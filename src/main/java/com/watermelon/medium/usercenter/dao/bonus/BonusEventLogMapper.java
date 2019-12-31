1ad3994c647cd50557d0290d3cdab6476cff7e6e
zhujiashuai <jiashuai.zhu@ishansong.com>
2019-12-31 13:48:27 +0800

    生成SQL

---
+++ src/main/java/com/watermelon/medium/usercenter/dao/bonus/BonusEventLogMapper.java
@@ -0,0 +1,7 @@
+package com.watermelon.medium.usercenter.dao.bonus;
+
+import com.watermelon.medium.usercenter.domain.entity.bonus.BonusEventLog;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BonusEventLogMapper extends Mapper<BonusEventLog> {
+}
---
+++ src/main/java/com/watermelon/medium/usercenter/dao/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.watermelon.medium.usercenter.dao.user;
+
+import com.watermelon.medium.usercenter.domain.entity.user.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
